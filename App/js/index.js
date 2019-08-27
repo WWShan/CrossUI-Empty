881586f1dcce2e67ec1135c5bda5258c31a39efc
WWShan <13079419@qq.com>
2019-08-27 14:00:09 +0800

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,40 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Div")
+                .setHost(host,"xui_ui_div17")
+                .setLeft("24.166666666666668em")
+                .setTop("12.5em")
+            );
+            
+            append(
+                xui.create("xui.UI.Element")
+                .setHost(host,"xui_ui_element1")
+                .setLeft("13.333333333333334em")
+                .setTop("26.666666666666668em")
+            );
+            
+            append(
+                xui.create("xui.UI.Icon")
+                .setHost(host,"xui_ui_icon1")
+                .setLeft("19.166666666666668em")
+                .setTop("15em")
+                .setImageClass("xuifont xui-icon-xui")
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input5")
+                .setDirtyMark(false)
+                .setLeft("19.166666666666668em")
+                .setTop("29.166666666666668em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("输入框")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
